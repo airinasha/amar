7476e6144deeb7bdbe66a4d0d417efcabc573f0d
airinasha <85106829+airinasha@users.noreply.github.com>
2022-07-19 16:20:09 +0600

    dubug changes settings

--- miniblog/settings.py
+++ miniblog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+o1ef$m#)&1^hsh#^5ax!9nym(s-tnz08jl%79n$715ub52s7g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['aaatest2.herokuapp.com']
 
